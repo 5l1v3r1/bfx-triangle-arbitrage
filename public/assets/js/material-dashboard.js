916391597128835970eb69f5531aaad8f21a0ce6
Ezra <a001gtb@gmail.com>
2019-11-17 04:46:33 +1300

    comment refactor

--- public/assets/js/material-dashboard.js
+++ public/assets/js/material-dashboard.js
@@ -404,7 +404,7 @@ md = {
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
+// n milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 
 function debounce(func, wait, immediate) {
